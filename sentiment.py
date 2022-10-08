adc8c1c29c9edad95a6485ae07352b11c1871ac7
gavramop <94786809+gavramop@users.noreply.github.com>
2022-10-08 17:11:54 +0300

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -22,7 +22,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 # Importing the dataset
 DATASET_COLUMNS=['target','ids','date','flag','user','text']
 DATASET_ENCODING = "ISO-8859-1"
-df = pd.read_csv (r'C:\Users\gogas\Desktop\Dataset2.csv' , encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+df = pd.read_csv (r'Dataset.csv' , encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
 print(df)
 
 
